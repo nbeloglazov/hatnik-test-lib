77b3b1b20c6c1de788badf43ee8a1696ce425a39
Nikita Beloglazov <nikelandjelo@gmail.com>
2014-09-28 23:18:26 +0100

    Update

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject com.nbeloglazov/hatnik-test-lib "0.1.0"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject com.nbeloglazov/hatnik-test-lib "0.1.5"
+  :description "Dummy library that can be released on any time. Used for testing Hatnik."
+  :url "http://hatnik.clojurecup.com"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
